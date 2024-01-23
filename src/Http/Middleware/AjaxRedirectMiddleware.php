5f0331d2ba8fcfe280c4dcdb852ac188366b192c
mintellity <mintellity@users.noreply.github.com>
2024-01-23 07:14:10 +0000

    Fix styling

--- src/Http/Middleware/AjaxRedirectMiddleware.php
+++ src/Http/Middleware/AjaxRedirectMiddleware.php
@@ -13,21 +13,21 @@ class AjaxRedirectMiddleware
     /**
      * Send redirect URL as JSON response if request comes from AJAX.
      *
-     * @param Request $request
-     * @param Closure(Request): (Response|RedirectResponse) $next
-     * @return Response|RedirectResponse|JsonResponse
+     * @param  Closure(Request): (Response|RedirectResponse)  $next
      */
     public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         // If the request is not an AJAX request, we will just return the response
-        if (!$request->ajax())
+        if (! $request->ajax()) {
             return $next($request);
+        }
 
         $response = $next($request);
 
         // If the response is not a redirect, we will just return the response
-        if (!$response instanceof RedirectResponse)
+        if (! $response instanceof RedirectResponse) {
             return $response;
+        }
 
         $targetUrl = $response->getTargetUrl();
 
