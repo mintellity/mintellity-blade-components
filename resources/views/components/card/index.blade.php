38d79cee474909ece7d9e52f3c63d7c4a0ac4318
Erik Sadewater <erik.sadewater@mintellity.com>
2024-07-24 15:50:16 +0200

    feat: Optimized card component

--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -1,7 +1,17 @@
-<div class="card">
-    {{ $header ?? '' }}
+@props([
+    'title' => null,
+])
 
-    <div class="card-body pt-5">
+<div class="card">
+    @if($title)
+        <x-mint::card.header>
+            <h5 class="card-title">{{ $title }}</h5>
+        </x-mint::card.header>
+    @else
+        {{ $header ?? '' }}
+    @endif
+
+    <div class="card-body">
         {{ $slot }}
     </div>
 
